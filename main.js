b686d4c5bb6b39b44f19766c12cd1abf20900350
mfer2228 <marifepa24@gmail.com>
2023-07-04 22:24:33 -0400

    correcciones

--- main.js
+++ main.js
@@ -3,38 +3,118 @@ console.log ("probandooo");
 
 alert ("Esta es una página por y para estudiantes");
 let nombre = prompt ( "¿Cuál es tu nombre?" );
+intencion()
 
-alert ("hola " + nombre + ", te ofrecemos los siguientes servicios:" + "\n1) Generador de citas APA    \n2) minijuego: Adivinanzas    \n3) Calculadora");
-
-
-
-do { 
-    intencion = prompt ("Ingresa  1, 2, o 3")
-             
-    switch (intencion) {
+ function  intencion () {
+  const intenc = prompt ("hola " + nombre + ", te ofrecemos los siguientes servicios:" + "\n1) Generador de citas APA    \n2) minijuego: Adivinanzas    \n3) Calculadora")  
+    switch (intenc) {
  case "1":
-    alert ( "Bienvenido al generador de citas bibliográficas web APA, por favor ingresa los datos que te pediremos a continuación")
-    let titulo = prompt("nombre de el artículo que quieres citar")
-    let sitio = prompt("nombre del sitio que contiene el artículo")
-    let apellido = prompt("Apellido del autor del artículo")
-    let inicial = prompt("Inicial del nombre del autor del artículo")
-    let fecha = prompt("Año en que el artículo fue publicado")
-    let consulta = prompt("Fecha en la cual el artículo fue consultado Ejemplo: 22 de mayo de 2019")
-    let url = prompt ("Por último, ingresa la url del sitio a citar")
+ 
+ let citas = [];
+  
+  function mostrarMenu() {
+  
+    const opcion = prompt("Ingrese el número de la opción deseada: \n ----- MENÚ ----- \n 1. Agregar cita \n 2. Ver citas en consola \n 3. Eliminar una cita \n 4. Volver al menú anterior"
+    );
+  
+    switch (opcion) {
+      case "1":
+        solicitarDatosCita()
+        mostrarMenu();
+        break;
 
-    alert ("tu cita en formato APA es: " + apellido + " " + inicial + " (" + fecha + ")" + " " + titulo + ". " + "Recuperado el " + consulta + ", de " + sitio  + url)
+      case "2":
+        mostrarCitas()
+        alert ("las citas se mostrarán en consola")
+        mostrarMenu();
+        break;
 
-    
+      case "3":
+        eliminarCita()
+        alert ("sus cambios serán visibles en consola")
+        mostrarCitas()
+        mostrarMenu();
+        break;
 
+      case "4":
+        intencion();
+        break;
+      default:
+        alert ("Opción inválida. Por favor, ingrese un número válido del menú.")
+        
+        mostrarMenu();
+    }
+  }
+  
+  function solicitarDatosCita () {
+      const autor = prompt("Ingrese el nombre completo del autor:");
+      const fecha = prompt("Ingrese la fecha de publicación (AAAA, MM DD):");
+      const titulo = prompt("Ingrese el título del artículo o libro:");
+      const fechaConsulta = prompt("Ingrese la fecha de consulta (AAAA, MM DD):");
+      const sitioWeb = prompt("Ingrese el nombre del sitio web:");
+      const url = prompt("Ingrese la URL del artículo:");
+      
+      // Formatear el nombre del autor
+      const partesAutor = autor.split(" ");
+      let nombreAutorFormateado = partesAutor[partesAutor.length - 1];
+      
+      for (let i = 0; i < partesAutor.length - 1; i++) {
+        nombreAutorFormateado += `, ${partesAutor[i].charAt(0)}.`;
+      }
+      
+      // Crear la cita en formato APA
+      const cita = `${nombreAutorFormateado} (${fecha}) "${titulo}". Recuperado el ${fechaConsulta} de ${sitioWeb} ${url}`;
+      
+      // Agregar la cita al array
+      citas.push(cita);
+  }  
+      // Preguntar si desea agregar otra cita
+      const agregarOtraCita = confirm("¿Desea agregar una cita?");
+  
+      if (agregarOtraCita) {
+        solicitarDatosCita();
+      } else {
+        mostrarMenu();
+      }
+   
+  
+  function mostrarCitas() {
+
+      console.log("Lista de citas:");
+      citas.forEach((cita, index) => {
+        console.log(`Cita ${index + 1}:`);
+        console.log(cita);
+        console.log("----------------------");
+      });  }
+  
+  function eliminarCita() {
+    const borrarCita = confirm("¿Desea borrar alguna cita?");
+  
+    if (borrarCita) {
+      const numeroCita = prompt("Ingrese el número de la cita que desea borrar:");
+      
+      // Verificar si el número de cita es válido
+      if (numeroCita >= 1 && numeroCita <= citas.length) {
+        citas.splice(numeroCita - 1, 1);
+        console.log("La cita ha sido borrada correctamente.");
+      } else {
+        console.log("Número de cita inválido.");
+      }
+    }
+  }
+  
+  mostrarMenu();
+ 
   break;
 
- case "2":
+  case "2":
     alert ("adivina adivinador...")
     numero = Math.random() * 10
     if (numero <= 5) {
     respuesta = prompt ("Si de Covid me curo en dos días y de meninguitis en 4, ¿en cuantos días me curo de sarampión?")
       if (respuesta == "3") {
         alert ("Felicidades!!")
+        intencion ()
       }
       
       else if (respuesta != "3", "salir", "esc") {
@@ -47,6 +127,7 @@ do {
         respuesta = prompt ("Tengo una caja musical donde entran palabras, pero no entran todas. Sol entra, pero Luna no. Dólar entra, pero Euro no, ¿Micrófono entra? ")
         if (respuesta == "si"){
             alert ("Felicidades " + nombre + " tu respuesta es correcta")
+            intencion ()
         }
         else if (respuesta !="si") {
             alert (" Mejor suerte la próxima :( ")
@@ -55,7 +136,7 @@ do {
     }
     break;
 
- case "3":
+  case "3":
  
  alert ("Hola, esta es una calculadora, no es mucho pero es trabajo honesto")
  let num1 = parseFloat(prompt("Introduce el primer número:"));
@@ -66,26 +147,27 @@ do {
  switch (operacion) {
   case "1":
     alert("La suma es: " + (num1 + num2));
+    intencion()
     break;
   case "2":
     alert("La resta es: " + (num1 - num2));
+    intencion()
     break;
   case "3":
     alert("La multiplicación es: " + (num1 * num2));
+    intencion()
     break;
   case "4":
     alert("La división es: " + (num1 / num2));
+    intencion()
     break;
   default:
     alert("Opción inválida");
 }
-
-
+ break;
 
  case "esc":
  alert ("Hasta luego, " + nombre)
- 
 
- }
-} while (intencion != "1", "2", "3", esc);  
-
+}
+}
